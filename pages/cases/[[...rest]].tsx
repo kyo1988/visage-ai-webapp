583590625921a3079863acccaccf1ee0c230422c
Kyo Harada <kh685mba@gmail.com>
2025-08-12 01:11:22 +0900

    feat: TypeScriptエラー

--- pages/cases/[[...rest]].tsx
+++ pages/cases/[[...rest]].tsx
@@ -1,1 +1,17 @@
-export const getServerSideProps=({resolvedUrl})=>({redirect:{destination:'/ja'+resolvedUrl,permanent:true}});export default function P(){return null}
+import type { GetServerSideProps, GetServerSidePropsContext } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const resolvedUrl = ctx.resolvedUrl ?? "/";
+  return {
+    redirect: {
+      destination: "/ja" + resolvedUrl,
+      permanent: true,
+    },
+  };
+};
+
+export default function P() {
+  return null;
+}
